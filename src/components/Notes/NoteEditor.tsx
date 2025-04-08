7d903b0c547dbe15ba60e0599d55af2b7d006d1f
chenryace <78716224+chenryace@users.noreply.github.com>
2025-04-09 05:06:04 +0800

    Add files via upload

--- src/components/Notes/NoteEditor.tsx
+++ src/components/Notes/NoteEditor.tsx
@@ -73,7 +73,7 @@ const NoteEditor = ({ note, onChange, onTitleChange, onSave }: NoteEditorProps) 
       <div className="editor-container">
         {(() => {
           const editor = useMarkdownEditor({
-            defaultValue: noteContent,
+            content: content,
             onChange: handleContentChange,
             autoFocus: true,
             defaultMode: "edit",
@@ -81,7 +81,7 @@ const NoteEditor = ({ note, onChange, onTitleChange, onSave }: NoteEditorProps) 
             locale: "zh",
             theme: "dark"
           });
-          return editor.dom;
+          return editor.render();
         })()}
       </div>
     </div>
