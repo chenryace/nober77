a824f2d013fdab81b45c03ffd264851969c88e0c
chenryace <78716224+chenryace@users.noreply.github.com>
2025-04-10 01:25:05 +0800

    Add files via upload

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,4 @@
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-  var prisma: PrismaClient | undefined
-}
-
-// 确保只在服务器端初始化Prisma
-const prisma = global.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma
-}
-
-// Change from named export to default export
-export default prisma;
+const prisma = new PrismaClient()
+export default prisma
