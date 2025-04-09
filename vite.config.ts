c83d027ba9ca27802bb054ba43dce83215e10116
chenryace <78716224+chenryace@users.noreply.github.com>
2025-04-09 10:42:33 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    tsDecorators: true
+  })],
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  esbuild: {
+    loader: 'tsx',
+    include: /\.(tsx?|jsx)$/,
+    exclude: [],
   }
 })
