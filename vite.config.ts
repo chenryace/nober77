e183b69a66dc89b19eb102cfb3c896891a1cfb8f
chenryace <78716224+chenryace@users.noreply.github.com>
2025-04-09 10:40:30 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@prisma/client': '/node_modules/@prisma/client'
-    }
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
   }
 })
